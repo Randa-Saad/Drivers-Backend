ff7b48bce328fb8eeacdf97a9c59638ecd64f822
Randa Zobair <Randa.Zobair@iqranetwork.com>
2023-05-20 00:50:51 +0300

    Add Procedures file as needed step before running

---
+++ Transflo/assets/Documentation.sql
@@ -0,0 +1,130 @@
+
+----1--Create Database
+--CREATE DATABASE Transflo_DB;
+
+-----------------------------------------------------------------------------------------
+----2-- Create Table
+--CREATE TABLE Drivers (
+--    Id INT IDENTITY ,
+--    FirstName nvarchar(20),
+--    LastName nvarchar(20),
+--    Email nvarchar(20),
+--    PhoneNumber nvarchar(20),
+--	PRIMARY KEY (Id)
+--);
+
+-----------------------------------------------------------------------------------------
+----3--Create Stored Procedures
+---1--Stored Procedure: Get drivers
+--Create PROC [DBO].[usp_Get_Drivers]
+--AS
+--BEGIN
+--	SELECT Id,FirstName,LastName,Email,PhoneNumber from DBO.Drivers WITH (NOLOCK)
+--End
+
+--------------------------------------------------------
+
+---2--Stored Procedure: Get driver by id
+--CREATE PROC [DBO].[usp_Get_DriverById]
+--(
+--	@Id INT
+--)
+--AS
+--BEGIN
+--SELECT Id,FirstName,LastName,Email,PhoneNumber from DBO.Drivers WITH (NOLOCK)
+--	WHERE ID=@Id
+--END
+
+--------------------------------------------------------
+
+---3--Stored Procedure: Insert driver
+--CREATE PROC [DBO].[usp_Insert_Driver]
+--(
+--@FirstName NVARCHAR(20),
+--@LastName NVARCHAR(20),
+--@Email NVARCHAR(20),
+--@PhoneNumber NVARCHAR(20)
+--)
+--AS 
+--BEGIN
+
+--BEGIN TRY
+--	BEGIN TRAN
+--	INSERT INTO DBO.Drivers(FirstName, LastName, Email, PhoneNumber)
+--		VALUES
+--		(
+--			@FirstName,
+--			@LastName,
+--			@Email,
+--			@PhoneNumber
+--		)
+--	COMMIT TRAN
+--END TRY
+--BEGIN CATCH
+--	ROLLBACK TRAN
+--END CATCH
+--END
+
+--------------------------------------------------------
+
+---4--Stored Procedure: Update driver
+--CREATE PROC [DBO].[usp_Update_Driver]
+--(
+--	@Id INT,
+--	@FirstName NVARCHAR(20),
+--	@LastName NVARCHAR(20),
+--	@Email NVARCHAR(20),
+--	@PhoneNumber NVARCHAR(20)
+--)
+--AS
+--BEGIN
+--Declare @RowCount INT = 0
+--	BEGIN TRY
+--	SET @RowCount = (SELECT COUNT(1) FROM DBO.Drivers WITH (NOLOCK) WHERE Id= @Id )
+
+--	if(@RowCount >0)
+--		BEGIN
+--			BEGIN TRAN
+--				UPDATE DBO.Drivers
+--					SET 
+--						FirstName = @FirstName, 
+--						LastName= @LastName,
+--						Email = @Email,
+--						PhoneNumber = @PhoneNumber 
+--						WHERE Id = @Id
+--			COMMIT TRAN
+--		END
+--	END TRY
+--BEGIN CATCH
+--	ROLLBACK TRAN
+--END CATCH
+--END
+
+--------------------------------------------------------
+
+---5--Stored Procedure: Delete driver
+--CREATE PROC [DBO].[usp_Delete_Driver]
+--(
+--	@Id INT
+--)
+--AS
+--BEGIN
+--Declare @RowCount INT = 0
+--	BEGIN TRY
+--	SET @RowCount = (SELECT COUNT(1) FROM DBO.Drivers WITH (NOLOCK) WHERE Id= @Id )
+
+--	if(@RowCount >0)
+--		BEGIN
+--			BEGIN TRAN
+--				DELETE FROM  DBO.Drivers
+					
+--					WHERE Id = @Id
+--			COMMIT TRAN
+--		END
+--	END TRY
+--BEGIN CATCH
+--	ROLLBACK TRAN
+--END CATCH
+--END
+-----------------------------------------------------------------------------------
+
